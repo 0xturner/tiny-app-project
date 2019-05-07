4eee648cd8999325b098c4da077ae81243430d83
Turner Schumann <turnerschumann@gmail.com>
2019-05-07 23:06:44 +0000

    Install body parser and add Get and Post rout for urls-new

--- express-server.js
+++ express-server.js
@@ -2,11 +2,18 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 app.get('/urls', function(req, res) {
     let templateVars = { urls: urlDatabase}
     res.render("urls-index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls-new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
